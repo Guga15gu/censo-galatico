80c946a976d1c513c81df5d44a721584b8accef4
guga15gu <48689692+Guga15gu@users.noreply.github.com>
2024-04-02 19:53:04 -0300

    tarefa 3: Clicar em botão e exibir dados do planeta

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let result = document.getElementById('result');
+let planet_buttons = document.getElementById('planet_buttons');
+printPlanets();
 
 async function printPlanets(){
 
@@ -8,10 +9,42 @@ async function printPlanets(){
     console.log(planets);
 
     planets.results.forEach(planet => {
-        let li = document.createElement('li');
+        let button = document.createElement('button');
+        button.textContent = planet.name;
+        button.addEventListener('click', () => {
+            getPlanetDetails(planet.name);
+          });
 
-        li.innerHTML = `<button>${planet.name}</button>`
-
-        result.appendChild(li);
+          planet_buttons.appendChild(button);
     });
 }
+
+async function getPlanetDetails(planetName) {
+
+    let planet_response = await fetch(`https://swapi.dev/api/planets/?search=${encodeURIComponent(planetName)}`);
+    let planet_data = await planet_response.json();
+    let planet = planet_data.results[0];
+    
+    let new_planet_details_div = document.createElement('div');
+    new_planet_details_div.id = 'planet_details';
+
+    let h2_name = document.createElement('h2');
+    h2_name.textContent = planet.name;
+    new_planet_details_div.appendChild(h2_name);
+
+    let div_climate = document.createElement('div');
+    div_climate.textContent = `Clima: ${planet.climate}`;
+    new_planet_details_div.appendChild(div_climate);
+
+    let div_population = document.createElement('div');
+    div_population.textContent = `População: ${planet.population}`;
+    new_planet_details_div.appendChild(div_population);
+
+    let div_terrain = document.createElement('div');
+    div_terrain.textContent = `Terreno: ${planet.terrain}`;
+    new_planet_details_div.appendChild(div_terrain);
+
+    let planet_details = document.getElementById('planet_details');
+    planet_details.replaceWith(new_planet_details_div);
+}
+  
